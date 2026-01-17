28703b793eb96c963e15986f650b856f497a9a40
Tushar Kumar <tusharkr1918@gmail.com>
2026-01-17 20:12:45 +0530

    Initial Spring Boot Setup

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.0.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.tushar"
+version = "0.0.1-SNAPSHOT"
+description = "TutorService"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springModulithVersion"] = "2.0.1"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.modulith:spring-modulith-starter-core")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.springframework.modulith:spring-modulith-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.modulith:spring-modulith-bom:${property("springModulithVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/dev/tushar/tutorservice/TutorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tushar.tutorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
